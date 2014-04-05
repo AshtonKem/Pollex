868ac0a7c23435a9cc7ce3b937934f04e6640b23
Ashton Kemerling <ashtonkemerling@gmail.com>
2014-04-04 21:54:43 -0600

    Init.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pollex
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject pollex "0.1.0-SNAPSHOT"
+  :description "Clojure Wrapper for Backbone"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [com.cemerick/clojurescript.test "0.3.0"]]
+  :dependencies [[org.clojure/clojurescript "0.0-2197"]])
