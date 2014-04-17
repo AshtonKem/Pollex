8ec8c3174dc0155148fa850ca300957e26dc5110
Ashton Kemerling <ashtonkemerling@gmail.com>
2014-04-16 21:56:09 -0600

    Update project.clj for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pollex "0.1.0-SNAPSHOT"
+(defproject AshtonKem/pollex "0.1.0-SNAPSHOT"
   :description "Clojure Wrapper for Backbone"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,6 +6,8 @@
   :plugins [[lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.3.0"]]
   :dependencies [[org.clojure/clojurescript "0.0-2197"]]
+  :hooks [leiningen.cljsbuild]
+  :aliases  {"release" ["do" "clean," "deploy" "clojars," "deploy" "releases"]}
   :cljsbuild {:builds [{:source-paths ["src" "test"]
                         :compiler {:output-to "target/cljs/testable.js"
                                    :externs ["resource/externs/"]
@@ -15,4 +17,9 @@
               :test-commands {"unit-tests" ["phantomjs" :runner
                                             "resources/underscore.js"
                                             "resources/backbone.js"
-                                            "target/cljs/testable.js"]}})
+                                            "target/cljs/testable.js"]}}
+  :pom-addition [:developers [:developer
+                             [:name "Ashton Kemerling"]
+                             [:url "http://ashtonkemerling.com"]
+                             [:email "ashtonkemerling@gmail.com"]
+                             [:timezone "-6"]]])
